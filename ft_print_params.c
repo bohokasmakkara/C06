c2076777805e20c062547896c6c877fe6560fa67
bohokasmakkara <162553789+bohokasmakkara@users.noreply.github.com>
2024-03-07 14:31:44 +0200

    Create ft_print_params.c
    • Create a program that displays its given arguments.
    • One per line, in the same order as in the command line.
    • It should display all arguments, except for argv[0].

---
+++ ft_print_params.c
@@ -0,0 +1,28 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int	main(int argc, char *argv[])
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	if (argc > 0)
+	{
+		while (argv[i] != NULL)
+		{
+			j = 0;
+			while (argv[i][j] != '\0')
+			{
+				ft_putchar(argv [i][j]);
+				j++;
+			}
+			i++;
+			ft_putchar('\n');
+		}
+	}
+}
